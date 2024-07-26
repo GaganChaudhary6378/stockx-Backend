11adf24fdf8006c3391a9f89b9bc4f447c1ca8b2
Gagan Chaudhary <singhgaganbtp@gmail.com>
2024-07-27 02:29:37 +0530

    changes in preferences api

--- src/controllers/preferences.controller.js
+++ src/controllers/preferences.controller.js
@@ -12,32 +12,34 @@ const preference = asyncHandler(async (req, res) => {
         throw new ApiError(400, "User does not exist");
     }
     const preferenceArray = user.favorites;
-    if (preferenceArray.length >= 3) {
-        throw new ApiError(400, "Cannot add more than 3 favorites");
-    } else {
-        const isExisting = preferenceArray.includes(id);
-        if (isExisting) {
-            // remove that id from the array
-            const index = preferenceArray.indexOf(id);
-            if (index > -1) {
-                preferenceArray.splice(index, 1);
-                await user.save({ validateBeforeSave: false });
-            }
+    const isExisting = preferenceArray.includes(id);
+    if (!isExisting) {
+        if (preferenceArray.length >= 3) {
+            throw new ApiError(400, "Cannot add more than 3 favorites");
         } else {
             preferenceArray.push(id);
             await user.save({ validateBeforeSave: false });
         }
-
+        res
+            .status(200)
+            .json(new ApiResponse(200, { preferenceArray }, "Preferences added successfully"));
     }
-    res
-        .status(200)
-        .json(new ApiResponse(200, { preferenceArray }, "Preferences added successfully"));
-
+    else {
+        const index = preferenceArray.indexOf(id);
+        if (index > -1) {
+            preferenceArray.splice(index, 1);
+            await user.save({ validateBeforeSave: false });
+        }
+        res
+            .status(200)
+            .json(new ApiResponse(200, { preferenceArray }, "Preferences modified successfully"));
+    }
 })
 
 const getAllPreferences = asyncHandler(async (req, res) => {
-    const { userId } = req.params;
-    const user = await UserInfo.findOne({ userId });
+    const { id } = req.params;
+    console.log(id);
+    const user = await UserInfo.findOne({ userId: id });
     if (!user) {
         throw new ApiError(400, "User not found");
     }
