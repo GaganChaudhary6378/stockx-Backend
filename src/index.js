5466af1a2b90f33db2ac09b5e715896731d2bacf
Gagan Chaudhary <singhgaganbtp@gmail.com>
2024-07-28 15:59:55 +0530

    index.js export

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import { app } from "./app.js";
+
 dotenv.config({
-    path: './.env'
-})
+  path: './.env'
+});
 
-
-
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
+// Function to start the server and export it
+async function startServer() {
+  try {
+    await connectDB();
+    const server = app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port: ${process.env.PORT || 8000}`);
+    });
+    return server; // Return the server instance
+  } catch (err) {
     console.log("MONGO db connection failed !!! ", err);
-})
+    throw err;
+  }
+}
+
+// Export the server as the default export
+export default startServer();
